55f3cd3dd9109b21ab7932c877f4be26fa04f606
Cho Yeonsoo <128021464+yeonsoo1010@users.noreply.github.com>
2024-07-05 03:34:27 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = (k- n/10) * 2000 + n * 12000;
+        // 서비스 음료 개수: n/10
+        // (k- n/10) * 2000 + n * 12000
+        
+        return answer;
+    }
+}
